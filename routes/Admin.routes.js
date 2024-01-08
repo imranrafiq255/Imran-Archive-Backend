2e22afafb3aa3e00e8968562d435f3162b7676a5
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-01-09 03:50:04 +0500

    jfkdjf

--- routes/Admin.routes.js
+++ routes/Admin.routes.js
@@ -4,7 +4,4 @@ const Router = express.Router();
 
 Router.route("/createadmin").post(createAdmin);
 Router.route("/login").post(adminLogIn);
-Router.route("/malik").get((req, res) => {
-  res.send("I am imran malik");
-});
 module.exports = Router;
