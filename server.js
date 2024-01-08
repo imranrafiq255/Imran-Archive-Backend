faa22efb4a2be67480002c75d64c815538716892
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-01-09 03:45:13 +0500

    jfkdjf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const cloudinary = require("cloudinary").v2;
-
+const fileModel = require("./models/File.models");
 const port = process.env.SERVER_PORT || 6500;
 const databaseConnection = require("./config/database");
 databaseConnection();
@@ -9,8 +9,19 @@ cloudinary.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-app.get("/", (req, res) => {
-  res.send("Deployed successfully");
+app.get("/", async (req, res) => {
+  try {
+    const files = await fileModel.find({});
+    return res.status(201).json({
+      success: true,
+      files,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
