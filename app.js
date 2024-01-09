51db3026d69d319f3ef8e414e18b480c2a1cfd44
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-01-10 02:49:55 +0500

    made few changes into admin controllers

--- app.js
+++ app.js
@@ -6,29 +6,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
 app.use(cookieParser());
-app.use(cors());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://imran-archive.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
   })
 );
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://imran-archive.vercel.app/"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 const Admin = require("./routes/Admin.routes");
 const File = require("./routes/File.routes");
 app.use("/api/v1/admin", Admin);
 app.use("/api/v1/file", File);
+
 module.exports = app;
