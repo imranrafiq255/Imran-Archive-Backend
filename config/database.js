b4a8002c0c93ffdc5da37f33fda5ffca2112e682
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-01-09 12:14:10 +0500

    first commit

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-const databaseConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then((con) => console.log(con.connection.host))
-    .catch((error) => console.log(error));
+
+const databaseConnection = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`Connected to MongoDB on host: ${connection.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
 };
+
 module.exports = databaseConnection;
