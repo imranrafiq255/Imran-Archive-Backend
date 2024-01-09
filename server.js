15e0bae189bfcb55a5c8eb6be3971696db1d91b9
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-01-09 12:34:07 +0500

    first commit

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ cloudinary.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-app.get("/", async (req, res) => {
-  try {
-    const files = await fileModel.find({});
-    return res.status(201).json({
-      success: true,
-      files,
-    });
-    return res.send("uploaded");
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
